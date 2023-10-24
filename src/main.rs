a5d6b8cd2e787ff99ed02c6936ef9adbf04585bb
wathigo <wathigosimon@gmail.com>
2023-10-24 21:15:57 +0300

    Store commandline args in variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    println!("Arguments passed are {:?}", arguments);
+
+    let search_query = &arguments[1];
+    let file_name = &arguments[2];
+
+    println!("Search query `{}`", search_query);
+    println!("In file `{}`", file_name);
 }
