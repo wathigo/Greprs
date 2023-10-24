be8dbf819c74b0d701b6a439674edf9e0763b8a7
wathigo <wathigosimon@gmail.com>
2023-10-24 21:30:45 +0300

    Implement open and read file operations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
 
     let search_query = &arguments[1];
     let file_name = &arguments[2];
-
     println!("Search query `{}`", search_query);
     println!("In file `{}`", file_name);
+
+    let mut f = File::open(file_name).expect("Couldn't open file!");
+
+    let mut content = String::new();
+    f.read_to_string(&mut content).expect("Couldn't read file contents!");
+
+    println!("File {} contains \n{}",file_name, content);
 }
